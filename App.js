672268d09d7c773c504dbb3e32362c9b47a0b5c2
Kazion500 <patrickckabwe@gmail.com>
2021-02-06 15:54:27 +0200

    add navigation tab and new header

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     height: "100%",
-    paddingVertical: Platform.OS == "android" ? 25 : 0,
+    paddingTop: Platform.OS == "android" ? 25 : 0,
   },
 
   logo: {
