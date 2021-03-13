8fdb2cbba4aeda61b70e8a616e3ff6aa70f8b5c6
Patrick Kabwe <patrick@propzi.com>
2021-03-13 20:05:29 +0200

    removed from getPropertyDetails

--- src/components/providers/AuthProvider.js
+++ src/components/providers/AuthProvider.js
@@ -1,37 +1,11 @@
-import React, { useState, useEffect } from "react";
-import { firebase, dbh } from "../../../firebase";
+import React, { useState } from "react";
+import { firebase } from "../../../firebase";
 
 export const AuthContext = React.createContext({});
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isFirstLaunch, setisFirstLaunch] = useState(false);
-
-  const [property, setproperty] = useState({});
-  const [address, setAddress] = useState("");
-
-  const state = {};
-
-  useEffect(() => {
-    async function getPropertyDetails() {
-      let propertyDetails = await dbh
-        .collection("UserDetails")
-        .doc(user.uid)
-        .collection("Property")
-        .get();
-      propertyDetails.forEach((doc) => {
-        let demoAddress = `${doc.data()?.streetNumber}, ${
-          doc.data()?.streetName
-        }, ${doc.data()?.city}`;
-
-        setAddress(demoAddress);
-        setPropertyData([...propertyData, doc.data()]);
-      });
-    }
-    getPropertyDetails();
-  }, [user]);
-
-  state["address"] = address;
 
   return (
     <AuthContext.Provider
