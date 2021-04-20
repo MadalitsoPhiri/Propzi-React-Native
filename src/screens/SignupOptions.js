095d254bad3e76e05d28df36a6f2cd803436307a
Madalitso Phiri <Madalitso.phiri150@gmail.com>
2021-04-20 02:03:29 +0200

    finished with facebook login

--- src/screens/SignupOptions.js
+++ src/screens/SignupOptions.js
@@ -19,6 +19,7 @@ import * as AppleAuthentication from 'expo-apple-authentication';
 import * as Google from 'expo-google-app-auth';
 import * as Facebook from 'expo-facebook';
 import Loader from "../components/Loader";
+import * as Crypto from 'expo-crypto';
 import { firebase,dbh} from "../../firebase";
 
 // TODO:// Configure the title
@@ -27,70 +28,184 @@ export default function SignupOptions({navigation}) {
   const [isLoading,setLoading] = useState(false);
 
     async function handleAppleClick(){
-      try {
-        const cred = await AppleAuthentication.signInAsync({
-          requestedScopes: [
-            AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
-            AppleAuthentication.AppleAuthenticationScope.EMAIL,
-          ],
+      setLoading(true)
+        const nonce = Math.random().toString(36).substring(2, 10);
+    
+        return Crypto.digestStringAsync(Crypto.CryptoDigestAlgorithm.SHA256, nonce)
+            .then((hashedNonce) =>
+                AppleAuthentication.signInAsync({
+                    requestedScopes: [
+                        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+                        AppleAuthentication.AppleAuthenticationScope.EMAIL
+                    ],
+                    nonce: hashedNonce
+                })
+            )
+            .then((appleCredential) => {
+                const { identityToken } = appleCredential;
+                const provider = new firebase.auth.OAuthProvider('apple.com');
+                const credential = provider.credential({
+                    idToken: identityToken,
+                    rawNonce: nonce
+                });
+                return firebase.auth().signInWithCredential(credential)
+                .then((credential)=>{
+                  //User Succsessfully signed in
+                  dbh.collection("UserDetails").doc(credential.user.uid).collection("User").get().then((docSnapshot) => {
+          if (docSnapshot.size == 0) {
+            dbh.collection(`UserDetails/${credential.user.uid}/User`).add({
+              fullName:credential.user.displayName,phone:credential.user.phoneNumber,email:credential.user.email,clientIsMobile:true
+          }).then((obj)=>{
+              
+          },(err)=>{
+         setLoading(false)
+         Alert.alert(
+          "Error",
+          err.message,
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel"
+            },
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ]
+        );
+          })
+          
+          }
+        
         });
-        // signed in
-        setLoading(true)
+        
+        
+              }).catch((error) => {
+                setLoading(false)
+                Alert.alert(
+                  "Error",
+                  error.message,
+                  [
+                    {
+                      text: "Cancel",
+                      onPress: () => console.log("Cancel Pressed"),
+                      style: "cancel"
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") }
+                  ]
+                );
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+              });
+                
+            })
+            .catch((error) => {
+                // ...
+                setLoading(false)
+                Alert.alert(
+                  "Error",
+                  error.message,
+                  [
+                    {
+                      text: "Cancel",
+                      onPress: () => console.log("Cancel Pressed"),
+                      style: "cancel"
+                    },
+                    { text: "OK", onPress: () => console.log("OK Pressed") }
+                  ]
+                );
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                // The email of the user's account used.
+                var email = error.email;
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential;
+                // ...
+            });
+  
+//       try {
+//         const cred = await AppleAuthentication.signInAsync({
+//           requestedScopes: [
+//             AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+//             AppleAuthentication.AppleAuthenticationScope.EMAIL,
+//           ],
+//         });
+//         // signed in
+//         setLoading(true)
 
         
-        firebase.auth().signInWithCredential(cred).then((credential)=>{
-          //User Succsessfully signed in
-          dbh.collection("UserDetails").doc(credential.user.uid).collection("User").get().then((docSnapshot) => {
-  if (docSnapshot.size == 0) {
-    dbh.collection(`UserDetails/${credential.user.uid}/User`).add({
-      fullName:credential.user.displayName,phone:credential.user.phoneNumber,email:credential.user.email,clientIsMobile:true
-  }).then((obj)=>{
+//         firebase.auth().signInWithCredential(cred).then((credential)=>{
+//           //User Succsessfully signed in
+//           dbh.collection("UserDetails").doc(credential.user.uid).collection("User").get().then((docSnapshot) => {
+//   if (docSnapshot.size == 0) {
+//     dbh.collection(`UserDetails/${credential.user.uid}/User`).add({
+//       fullName:credential.user.displayName,phone:credential.user.phoneNumber,email:credential.user.email,clientIsMobile:true
+//   }).then((obj)=>{
       
-  },(err)=>{
- setLoading(false)
- setError(err)
-  })
+//   },(err)=>{
+//  setLoading(false)
+//  setError(err)
+//   })
   
-  }
+//   }
 
-});
+// });
 
 
-      }).catch((error) => {
-        setLoading(false)
+//       }).catch((error) => {
+//         setLoading(false)
+//         Alert.alert(
+//           "Error",
+//           error.message,
+//           [
+//             {
+//               text: "Cancel",
+//               onPress: () => console.log("Cancel Pressed"),
+//               style: "cancel"
+//             },
+//             { text: "OK", onPress: () => console.log("OK Pressed") }
+//           ]
+//         );
+//         // Handle Errors here.
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//         // The email of the user's account used.
+//         var email = error.email;
+//         // The firebase.auth.AuthCredential type that was used.
+//         var credential = error.credential;
+//         // ...
+//       });
+//       } catch (e) {
+//         if (e.code === 'ERR_CANCELED') {
+//           // handle that the user canceled the sign-in flow
+//           setLoading(false)
+//         }
+//         else {
+//           setLoading(false)
+//           Alert.alert(
+//             "Error",
+//             e.message,
+//             [
+//               {
+//                 text: "Cancel",
+//                 onPress: () => console.log("Cancel Pressed"),
+//                 style: "cancel"
+//               },
+//               { text: "OK", onPress: () => console.log("OK Pressed") }
+//             ]
+//           );
       
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
-      } catch (e) {
-        if (e.code === 'ERR_CANCELED') {
-          // handle that the user canceled the sign-in flow
-        }
-        else {
-          Alert.alert(
-            "Alert Title",
-            e.message,
-            [
-              {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-              },
-              { text: "OK", onPress: () => console.log("OK Pressed") }
-            ]
-          );
-      
-        }
-      }
+//         }
+//       }
     }
    
     handlepress = async()=> {
+      setLoading(true)
       try {
           await Facebook.initializeAsync({
             appId: Platform.OS === 'ios' ? '845016889691335':'185801686567133',
@@ -175,6 +290,7 @@ export default function SignupOptions({navigation}) {
         return { cancelled: true };
         }
       } catch ({ message }) {
+        setLoading(false)
         Alert.alert(
           "Alert Title",
           message,
